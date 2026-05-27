4eb311ca1e5a01d8c5554bf21b00c9418fa49540
kumar gaurav <kgaurav6791@gmail.com>
2026-05-27 12:07:22 +0530

    Fix npm peer dependency install error for netlify

--- .history/src/components/PhotoGallery_20220621140323.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PhotoGallery = () => {
-  return <div>PhotoGallery yo</div>;
-};
-
-export default PhotoGallery;
--- .history/src/components/popup/popup_20220730172942.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Popup.css";
-const popup = () => {
-  return <div>popup</div>;
-};
-
-export default popup;
